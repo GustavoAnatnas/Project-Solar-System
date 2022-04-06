c77ebd6aa5c7df7f9261b92d667e33e45bfdad94
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-06 20:00:26 -0300

    Fixa erros Lint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 import PlanetCard from './components/PlanetCard';
--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
-    const { name, year, country, destination} = this.props;
+    const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
         <p data-testid="mission-name">{ name }</p>
@@ -13,5 +14,12 @@ class MissionCard extends React.Component {
     );
   }
 }
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
 
 export default MissionCard;
