3d2058fdfb5ac083b7ca104cf04cc152889b1b25
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-04-06 16:49:56 -0300

    Desafio 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{this.props.planetName}</p>
+        <img src={ this.props.planetImage } alt={ `Planeta ${this.props.planetName}` } />
+      </div>
+    );
+  }
+}
+
+export default PlanetCard;
